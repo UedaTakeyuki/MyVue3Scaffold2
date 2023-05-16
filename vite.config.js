bee36a6a02a75661f2963b0f76e5c480ba04cd23
Takeyuki UEDA <ueda@latelierdueda.com>
2023-05-16 21:18:39 +0900

    first working version

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteIgnoreStaticImport(["vuetify"]) // <---- pass in the modules you want to ignore
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -24,9 +27,39 @@ export default defineConfig({
         analyze(), 
         visualizer(),
       ],
-/*      globals: {
-        'vue': 'Vue',
-      },*/
     },
   },
 })
+// https://stackoverflow.com/questions/76259677/vite-dev-server-throws-error-when-resolving-external-path-from-importmap/76262204#76262204
+function viteIgnoreStaticImport(importKeys) {
+  return {
+    name: "vite-plugin-ignore-static-import",
+    enforce: "pre",
+    // 1. insert to optimizeDeps.exclude to prevent pre-transform
+    config(config) {
+      config.optimizeDeps = {
+        ...(config.optimizeDeps ?? {}),
+        exclude: [...(config.optimizeDeps?.exclude ?? []), ...importKeys],
+      };
+    },
+    // 2. push a plugin to rewrite the 'vite:import-analysis' prefix
+    configResolved(resolvedConfig) {
+      const VALID_ID_PREFIX = `/@id/`;
+      const reg = new RegExp(
+        `${VALID_ID_PREFIX}(${importKeys.join("|")})`,
+        "g"
+      );
+      resolvedConfig.plugins.push({
+        name: "vite-plugin-ignore-static-import-replace-idprefix",
+        transform: (code) =>
+          reg.test(code) ? code.replace(reg, (m, s1) => s1) : code,
+      });
+    },
+    // 3. rewrite the id before 'vite:resolve' plugin transform to 'node_modules/...'
+    resolveId: (id) => {
+      if (importKeys.includes(id)) {
+        return { id, external: true };
+      }
+    },
+  };
+}
